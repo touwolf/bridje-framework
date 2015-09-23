981b183d865b53ad20b0df052996399b4d4272c5
Alejandro Ferrandiz Fonseca <acksecurity@hotmail.com>
2015-09-23 00:52:35 -0500

    Working on ContextListener

--- bridje-core/bridje-ioc/src/main/java/org/bridje/ioc/ClassRepository.java
+++ bridje-core/bridje-ioc/src/main/java/org/bridje/ioc/ClassRepository.java
@@ -1,6 +1,9 @@
 /*
  * Copyright 2015 Bridje Framework.
  *
+ * Alejandro Ferrandiz (acksecurity[at]hotmail.com)
+ * Gilberto Vento (gilberto.vento[at]gmail.com)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,7 +23,6 @@ import java.lang.annotation.Annotation;
 
 /**
  *
- * @author Gilberto
  */
 public interface ClassRepository
 {
