859fb2a481c2461e30b18bd2064529b7a2d5ed92
gilbertovento <gilberto.vento@gmail.com>
2015-12-28 21:05:53 -0500

    Documenting the configuration api.

--- bridje-core/src/main/java/org/bridje/core/cfg/ConfigRepository.java
+++ bridje-core/src/main/java/org/bridje/core/cfg/ConfigRepository.java
@@ -16,15 +16,66 @@
 
 package org.bridje.core.cfg;
 
+/**
+ * An interface that every configuration repository must implements.
+ *
+ * The ConfigService will use all the config repositories components to find and
+ * saves configurations.
+ */
 public interface ConfigRepository
 {
+    /**
+     * Finds the default configuration object by the given class.
+     *
+     * @param <T> The type of the configuration class.
+     * @param configClass The class of the configuration object to be obtained.
+     * @return An instance of configClass class witch represents the default
+     * configuration object of that class.
+     */
     <T> T findConfig(Class<T> configClass);
-    
+
+    /**
+     * Finds the configuration object named by the configName parameter that
+     * instantiates the especified class.
+     *
+     * @param <T> The type of the configuration to be obtained.
+     * @param configName The name of the configuration.
+     * @param configClass The class of the configuration.
+     * @return An instance of configClass class witch represents the
+     * configuration object named by the configName parameter.
+     */
     <T> T findConfig(String configName, Class<T> configClass);
 
+    /**
+     * Saves the default configuration instance to the repository witch may be
+     * obtained by the findConig method with the class of the saved object.
+     *
+     * @param <T> The type of the configuration object.
+     * @param newConfig The configuration object to be save as the default
+     * instance of the object´s class config.
+     * @return The newConfig object passed to this method.
+     */
     <T> T saveConfig(T newConfig);
-    
+
+    /**
+     * Saves the named configuration instance to the repository witch may be
+     * obtained by the findConig method with the name and class of the saved
+     * object.
+     *
+     * @param <T> The type of the configuration object.
+     * @param configName The name for this configuration instance.
+     * @param newConfig The configuration object to be save as the named
+     * instance of the object´s class config.
+     * @return The newConfig object passed to this method.
+     */
     <T> T saveConfig(String configName, T newConfig);
 
+    /**
+     * Especifies when ever this repository allows to save configuration
+     * instances or not.
+     *
+     * @return true this repository allows to save configuration instances,
+     * false otherwise.
+     */
     boolean canSave();
 }
