ca60d337581012fef46ca489267b805774ebf6e4
Gilberto Vento <gilberto.vento@gmail.com>
2016-04-23 00:30:44 -0500

    Documenting.

--- bridje-vfs/src/main/java/org/bridje/vfs/VfsService.java
+++ bridje-vfs/src/main/java/org/bridje/vfs/VfsService.java
@@ -18,31 +18,113 @@ package org.bridje.vfs;
 
 import java.util.List;
 
+/**
+ * This interface represents the virtual file system for bridje, it provides
+ * methods to mount and file files and folder into the system.
+ */
 public interface VfsService
 {
+    /**
+     * Finds the virtual folder by the given path.
+     *
+     * @param path the path of the folder.
+     * @return The folder finded or null if it does not exists.
+     */
     VirtualFolder findFolder(String path);
 
+    /**
+     * Finds the virtual folder by the given path.
+     *
+     * @param path the path of the folder.
+     * @return The folder finded or null if it does not exists.
+     */
     VirtualFolder findFolder(Path path);
 
+    /**
+     * Finds the virtual file by the given path.
+     *
+     * @param path the path of the file.
+     * @return The file finded or null if it does not exists.
+     */
     VirtualFile findFile(String path);
 
+    /**
+     * Finds the virtual file by the given path.
+     *
+     * @param path the path of the file.
+     * @return The file finded or null if it does not exists.
+     */
     VirtualFile findFile(Path path);
 
+    /**
+     * List all the folders children to this folder.
+     *
+     * @return The childs folders of this folder.
+     */
     List<VirtualFolder> listFolders();
 
+    /**
+     * List all the files children to this folder.
+     *
+     * @return The childs files of this folder.
+     */
     List<VirtualFile> listFiles();
-    
+
+    /**
+     * List all the folders children to this folder.
+     *
+     * @param query The regular expression to filter the folders names.
+     * @return The childs folders of this folder.
+     */
     List<VirtualFolder> listFolders(String query);
 
+    /**
+     * List all the files children to this folder.
+     *
+     * @param query The regular expression to filter the files names.
+     * @return The childs files of this folder.
+     */
     List<VirtualFile> listFiles(String query);
 
+    /**
+     * Mounts a new source into the given path.
+     *
+     * @param path The path to mount the source.
+     * @param source The virtual file system source object to be mounted.
+     */
     void mount(Path path, VfsSource source);
 
+    /**
+     * Navigates the vfs tree from this folder, matching all the child folders
+     * recursively.
+     *
+     * @param visitor The visitor to accept the files.
+     */
     void travel(VirtualFileVisitor visitor);
 
+    /**
+     * Navigates the vfs tree from this folder, matching all the child folders
+     * recursively.
+     *
+     * @param visitor The visitor to accept the folders.
+     */
     void travel(VirtualFolderVisitor visitor);
-    
+
+    /**
+     * Navigates the vfs tree from this folder, matching all the child folders
+     * recursively.
+     *
+     * @param visitor The visitor to accept the files.
+     * @param query The regular expresion to math the files full path.
+     */
     void travel(VirtualFileVisitor visitor, String query);
 
+    /**
+     * Navigates the vfs tree from this folder, matching all the child folders
+     * recursively.
+     *
+     * @param visitor The visitor to accept the folders.
+     * @param query The regular expresion to math the folders full path.
+     */
     void travel(VirtualFolderVisitor visitor, String query);
 }
