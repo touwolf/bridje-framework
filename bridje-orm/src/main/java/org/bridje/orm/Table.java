86ec57cf41d221eb82b0b5b8b3909f192330abd9
Gilberto Vento <gilberto.vento@gmail.com>
2016-04-23 00:20:46 -0500

    Documenting.

--- bridje-orm/src/main/java/org/bridje/orm/Table.java
+++ bridje-orm/src/main/java/org/bridje/orm/Table.java
@@ -22,41 +22,42 @@ import java.util.List;
  * Represents a database table attached to an entity of the model. This object
  * can be user to query the especific table and return the entitys atached to
  * it.
- * 
+ *
  * @param <T> The type of the entity that the table is mapped to.
  */
 public interface Table<T>
 {
     /**
      * Gets the entity class mapped to this table.
-     * 
+     *
      * @return A class of type T representing the class of the entity.
      */
     Class<T> getEntity();
 
     /**
      * The table name as it is created in the database.
-     * 
+     *
      * @return The name for this table.
      */
     String getName();
 
     /**
      * All the columns of the table.
-     * 
+     *
      * @return The list of all the columns for this table.
      */
     List<TableColumn<T, ?>> getColumns();
 
     /**
-     * 
-     * @return 
+     * The key column for this table.
+     *
+     * @return A table column wich is the primary key column for this table.
      */
     TableColumn<T, ?> getKey();
 
     /**
      * Finds the column for the given field name.
-     * 
+     *
      * @param fieldName The name of the field attached to the column.
      * @return The column object for the given field.
      */
