968070de2841f1d0b780f28a538f1536750cb5f9
gorums <acksecurity@hotmail.com>
2016-07-15 00:19:48 -0500

    Fix import class

--- bridje-webui/src/main/java/org/bridje/webui/Button.java
+++ bridje-webui/src/main/java/org/bridje/webui/Button.java
@@ -16,13 +16,12 @@
 
 package org.bridje.webui;
 
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlValue;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import org.bridje.web.view.UIExpression;
-import org.bridje.web.view.UIExpressionAdapter;
-import org.bridje.web.view.WebComponent;
+import org.bridje.web.view.comp.UIExpression;
+import org.bridje.web.view.comp.UIExpressionAdapter;
+import org.bridje.web.view.comp.WebComponent;
 
 @XmlRootElement(name = "button")
 public class Button extends WebComponent
