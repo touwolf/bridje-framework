9188e743cd06d592fce59ec4aeee5f8135316509
gilbertovento <gilberto.vento@gmail.com>
2016-09-26 21:14:32 -0500

    Adding templates to the ORM code generation.

--- bridje-orm/src/main/resources/BRIDJE-INF/srcgen/CodeGenerator.groovy
+++ bridje-orm/src/main/resources/BRIDJE-INF/srcgen/CodeGenerator.groovy
@@ -49,6 +49,27 @@ def toSqlName = { name ->
     result;
 };
 
+def loadCustomTypes = { ->
+    def dtList = tools.loadXmlResources("BRIDJE-INF/orm/datatypes.xml");
+    def dataTypes = [:];
+    dtList.each{ dtData ->
+        dtData.'*'.each{ dt ->
+            def dataType = [:];
+            dataType['name'] = dt.name();
+            dataType['class'] = dt.'@class'.text();
+            dataType['adapter'] = dt.'@adapter'.text();
+            dataType['sqlType'] = dt.'@sqlType'.text();
+            dataType['columnType'] = dt.'@columnType'.text();
+            dataTypes[dataType['name']] = dataType;
+        };
+    };
+    dataTypes;
+};
+
+def customTypes = loadCustomTypes();
+def numberTypes = ['integer', 'byte', 'short', 'long', 'float', 'double'];
+def stringTypes = ['string'];
+
 def findJavaType = { field ->
     def result = field['fieldType'].capitalize();
     if(field["isRelation"])
@@ -122,90 +143,129 @@ def findFieldDescription = { field, model ->
     field['description'];
 };
 
-def loadCustomTypes = { ->
-    def dtList = tools.loadXmlResources("BRIDJE-INF/orm/datatypes.xml");
-    def dataTypes = [:];
-    dtList.each{ dtData ->
-        dtData.'*'.each{ dt ->
-            def dataType = [:];
-            dataType['name'] = dt.name();
-            dataType['class'] = dt.'@class'.text();
-            dataType['adapter'] = dt.'@adapter'.text();
-            dataType['sqlType'] = dt.'@sqlType'.text();
-            dataType['columnType'] = dt.'@columnType'.text();
-            dataTypes[dataType['name']] = dataType;
-        };
+def readFieldData = { entity, fieldNode, model ->
+    def field = [:];
+    field['name'] = fieldNode.'@name'.text();
+    field['column'] = fieldNode.'@column'.text();
+    field['column'] = findColumnName(field);
+    field['fieldType'] = fieldNode.name();
+    field['sqlType'] = fieldNode.'@sqlType'.text();
+    field['adapter'] = fieldNode.'@adapter'.text();
+    field['length'] = fieldNode.'@length'.text();
+    field['isIndexed'] = fieldNode.'@indexed'.text() == "true";
+    field['isKey'] = fieldNode.'@key'.text() == "true" ;
+    field["isRelation"] = field['fieldType'] == 'relation';
+    if(field["isRelation"])
+        field['with'] = fieldNode.'@with'.text();
+    field['isEnum'] = field['fieldType'] == 'enum';
+    if(field["isEnum"])
+        field['type'] = fieldNode.'@type'.text();
+    field['isNumber'] = numberTypes.contains(field['fieldType']);
+    field['isAutoIncrement'] = fieldNode.'@autoIncrement'.text() == "true";
+    if(field['isAutoIncrement'])
+        field['isKey'] = true;
+    if(field['isKey'])
+        entity['keyField'] = field;
+    field['isString'] = stringTypes.contains(field['fieldType']);
+    field['isCustom'] = customTypes[field['fieldType']] != null;
+    if(field["isCustom"])
+    {
+        field['customType'] = customTypes[field['fieldType']];
+        if(field['adapter'] == "")
+            field['adapter'] = field['customType']['adapter'];
+        if(field['sqlType'] == "")
+            field['sqlType'] = field['customType']['sqlType'];
+    }
+    field['javaType'] = findJavaType(field);
+    field['tableColumn'] = findTableColumn(field);
+    field['description'] = fieldNode.'@description'.text();
+    field['description'] = findFieldDescription(entity, model);
+    field;
+};
+
+def readEntityFields = { entityNode, entity, model ->
+    entity['fields'] = [];
+    entity['fieldsMap'] = [:];
+    entityNode.'fields'.'*'.each{ fieldNode ->
+        def field = readFieldData( entity, fieldNode, model );
+
+        entity['fields'] << field;
+        entity['fieldsMap'][field['name']] = field;
     };
-    dataTypes;
+    entity;
+};
+
+def readEntityData = { entityNode, model ->
+    def entity = [:];
+
+    entity['package'] = model['package'];
+    entity['name'] = entityNode.'@name'.text();
+    entity['table'] = entityNode.'@table'.text();
+    entity['table'] = findTableName(entity, model);
+    entity['base'] = entityNode.'@base'.text();
+    entity['fullName'] = entity['package'] + "." + entity['name'];
+    entity['description'] = entityNode.'@description'.text();
+    entity['description'] = findEntityDescription(entity, model);
+    entity;
+};
+
+def applyBaseTemplate;
+applyBaseTemplate = { entity, templates ->
+
+    if(entity['base'] != "")
+    {
+        def tmpl = templates[entity['base']];
+        if(tmpl != null)
+        {
+            applyBaseTemplate(tmpl, templates);
+
+            if(entity['description'] == null || entity['description'].trim() == "")
+            {
+                entity['description'] = tmpl['description'];
+            }
+            def newFields = [];
+            tmpl['fields'].each{ field ->
+                if(entity['fieldsMap'][field['name']] == null)
+                {
+                    newFields << field;
+                    entity['fieldsMap'][field['name']] = field;
+                }
+            };
+            entity['fields'].each{ field -> 
+                newFields << field;
+            };
+            entity['fields'] = newFields;
+            if(entity['keyField'] == null && tmpl['keyField'] != null)
+            {
+                entity['keyField'] = tmpl['keyField'];
+            }
+        }
+    }
 };
 
 def generateEntitys = { ->
     if(tools.fileExists("orm.xml"))
     {
-        def customTypes = loadCustomTypes();
-        def numberTypes = ['integer', 'byte', 'short', 'long', 'float', 'double'];
-        def stringTypes = ['string'];
-
         def ormData = tools.loadXmlFile("orm.xml");
         def model = [:];
         model['package'] = ormData.'@package'.text();
         model['defEntityDesc'] = ormData.'@defEntityDesc'.text();
         model['tablePrefix'] = ormData.'@tablePrefix'.text();
+        def templates = [:];
+
+        ormData.'templates'.'*'.each{ entityNode ->
+
+            def entity = readEntityData(entityNode, model);
+            entity = readEntityFields(entityNode, entity, model);
+
+            templates[entity['name']] = entity;
+        };
 
         ormData.'entities'.'*'.each{ entityNode ->
 
-            def entity = [:];
-            entity['package'] = model['package'];
-            entity['name'] = entityNode.'@name'.text();
-            entity['table'] = entityNode.'@table'.text();
-            entity['table'] = findTableName(entity, model);
-            entity['fullName'] = entity['package'] + "." + entity['name'];
-            entity['description'] = entityNode.'@description'.text();
-            entity['description'] = findEntityDescription(entity, model);
-
-            entity['fields'] = [];
-            entity['fieldsMap'] = [:];
-            entityNode.'fields'.'*'.each{ fieldNode ->
-                def field = [:];
-                field['name'] = fieldNode.'@name'.text();
-                field['column'] = fieldNode.'@column'.text();
-                field['column'] = findColumnName(field);
-                field['fieldType'] = fieldNode.name();
-                field['sqlType'] = fieldNode.'@sqlType'.text();
-                field['adapter'] = fieldNode.'@adapter'.text();
-                field['length'] = fieldNode.'@length'.text();
-                field['isIndexed'] = fieldNode.'@indexed'.text() == "true";
-                field['isKey'] = fieldNode.'@key'.text() == "true" ;
-                field["isRelation"] = field['fieldType'] == 'relation';
-                if(field["isRelation"])
-                    field['with'] = fieldNode.'@with'.text();
-                field['isEnum'] = field['fieldType'] == 'relation';
-                if(field["isEnum"])
-                    field['type'] = fieldNode.'@type'.text();
-                field['isNumber'] = numberTypes.contains(field['fieldType']);
-                field['isAutoIncrement'] = fieldNode.'@autoIncrement'.text() == "true";
-                if(field['isAutoIncrement'])
-                    field['isKey'] = true;
-                if(field['isKey'])
-                    entity['keyField'] = field;
-                field['isString'] = stringTypes.contains(field['fieldType']);
-                field['isCustom'] = customTypes[field['fieldType']] != null;
-                if(field["isCustom"])
-                {
-                    field['customType'] = customTypes[field['fieldType']];
-                    if(field['adapter'] == "")
-                        field['adapter'] = field['customType']['adapter'];
-                    if(field['sqlType'] == "")
-                        field['sqlType'] = field['customType']['sqlType'];
-                }
-                field['javaType'] = findJavaType(field);
-                field['tableColumn'] = findTableColumn(field);
-                field['description'] = fieldNode.'@description'.text();
-                field['description'] = findFieldDescription(entity, model);
-
-                entity['fields'] << field;
-                entity['fieldsMap'][field['name']] = field;
-            };
+            def entity = readEntityData(entityNode, model);
+            entity = readEntityFields(entityNode, entity, model);
+            applyBaseTemplate(entity, templates);
 
             def data = ['entity':entity];
             tools.generateClass(entity['fullName'], "orm/Entity.ftl", data);
