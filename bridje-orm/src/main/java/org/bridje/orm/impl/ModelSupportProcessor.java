a587b924c70be59b96e7fb467f1d7623f1ca4576
gilberto.vento <gilberto.vento@gmail.com>
2017-06-08 13:31:42 -0500

    Fix to the ORM model support.

--- bridje-orm/src/main/java/org/bridje/orm/impl/ModelSupportProcessor.java
+++ bridje-orm/src/main/java/org/bridje/orm/impl/ModelSupportProcessor.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.bridje.orm;
+package org.bridje.orm.impl;
 
 import java.io.IOException;
 import javax.annotation.processing.SupportedAnnotationTypes;
