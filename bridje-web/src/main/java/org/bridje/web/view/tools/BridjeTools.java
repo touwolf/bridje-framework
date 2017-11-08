a5c1400358131b380a02b5c43f28839670251661
gilberto.vento <gilberto.vento@gmail.com>
2017-11-08 15:17:58 -0500

    Adding random id.

---
+++ bridje-web/src/main/java/org/bridje/web/view/tools/BridjeTools.java
@@ -0,0 +1,29 @@
+
+package org.bridje.web.view.tools;
+
+import org.bridje.web.view.themes.ThemeTool;
+import org.bridje.ioc.Component;
+import org.bridje.ioc.thls.Thls;
+import org.bridje.web.WebScope;
+
+@Component
+@ThemeTool(name = "bridje")
+public class BridjeTools
+{
+    public String getRandomId()
+    {
+        return Thls.get(RandomIdGenerator.class).randomId();
+    }
+
+    public String getPath()
+    {
+        String pathStr = Thls.get(WebScope.class).getPath();
+        return pathStr;
+    }
+
+    public String getOrigPath()
+    {
+        String pathStr = Thls.get(WebScope.class).getOrigPath();
+        return pathStr;
+    }
+}
