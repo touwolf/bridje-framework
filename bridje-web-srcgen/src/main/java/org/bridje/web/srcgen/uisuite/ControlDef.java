28eed9a5d087099ca3b9dce59d3180e09b078180
gilbertovento <gilberto.vento@gmail.com>
2017-11-09 20:04:11 -0500

    Reestructuring UISUite generation.

--- bridje-web-srcgen/src/main/java/org/bridje/web/srcgen/uisuite/ControlDef.java
+++ bridje-web-srcgen/src/main/java/org/bridje/web/srcgen/uisuite/ControlDef.java
@@ -16,8 +16,6 @@
 
 package org.bridje.web.srcgen.uisuite;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -28,11 +26,6 @@ import javax.xml.bind.annotation.XmlAttribute;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ControlDef extends BaseControlDef
 {
-    private ControlWorkFlow input;
-
-    @XmlAttribute
-    private String base;
-
     @XmlAttribute
     private boolean isTransient;
 
@@ -49,65 +42,6 @@ public class ControlDef extends BaseControlDef
     public void setIsTransient(boolean isTransient)
     {
         this.isTransient = isTransient;
-    }
-
-    /**
-     * The parent for this control, by default it is "Control".
-     * 
-     * @return The name of the parent control.
-     */
-    public String getBaseName()
-    {
-        if (base == null) base = "Control";
-        return base;
-    }
-
-    /**
-     * Gets all the parents control of this control.
-     * 
-     * @return The list with the parents control of this control.
-     */
-    public List<ControlDef> getAllBase()
-    {
-        List<ControlDef> result = new ArrayList<>();
-        ControlDef currBase = getBase();
-        while(currBase != null)
-        {
-            result.add(currBase);
-            currBase = currBase.getBase();
-        }
-        return result;
-    }
-
-    /**
-     * The parent for this control, by default it is "Control".
-     * 
-     * @param base The name of the parent control.
-     */
-    public void setBaseName(String base)
-    {
-        this.base = base;
-    }
-
-    /**
-     * The flow for reading the input of the control.
-     * 
-     * @return The read input flow for this control.
-     */
-    public ControlWorkFlow getInput()
-    {
-        return input;
-    }
-
-    /**
-     * The base control for this control.
-     * 
-     * @return The Control that is the parent of this control.
-     */
-    public ControlDef getBase()
-    {
-        if(base == null || base.equals("Control")) return null;
-        return getUISuite().getControls().stream().filter(p -> p.getName().equalsIgnoreCase(base)).findFirst().orElse(null);
     }
 
     /**
